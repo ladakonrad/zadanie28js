cb9222f4d9fad618a5dd9f5ecee4f46a9c90acc7
Konrad <ladakonrad@gmail.com>
2018-10-15 21:42:11 +0200

    dodano zadanie28js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function check(n) {
+    if (n>=100 && n<=200)
+        console.log('Liczba znajduje się w przedziale')
+    else
+        console.log('Liczba nie znajduje się w przedziale')
+}
+
+check(199);
